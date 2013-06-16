b6985a341f94f0306ae37e3fd20ebe0bee910abe
David Lin <wildflame@126.com>
2013-06-17 01:34:39 +0800

    Update Video

---
+++ _posts/posts/Dragon.rb
@@ -0,0 +1,34 @@
+---
+
+
+---
+##GRE®測驗(The GRE® revised General Test)簡介
+
+GRE® 測驗全名為Graduate Record Examinations®，係由GRE®委員會委託美國教育測驗服務社(Educational Testing Service, ETS®) 舉辦的世界性測驗，成績為美國各大學研究所或研究機構的申請入學參考條件之一。
+
+The GRE® revised General Test 是經由網際網路方式進行，在臺灣、香港、中國大陸及南韓每個月舉行1-3次的測驗。測驗項目包含：分析寫作(Analytical Writing)、語文(Verbal Reasoning)及計量(Quantitative Reasoning)。
+
+## 題型
+
+測驗共分6個Section，全程約需4.5小時，測驗程序如下：（除寫作外，語文及計量出現的次序不固定）
+
+  	兩篇寫作（各30分鐘作答）
+  	語文 (Verbal Reasoning)一個部分（30分鐘作答）
+  	計量 (Quantitative Reasoning)一個部分（35分鐘作答）
+  	------- 10分鐘的休息時間（非強制性 ）-------
+  	語文 (Verbal Reasoning)一個部分（30分鐘作答）
+  	計量 (Quantitative Reasoning)一個部分（35分鐘作答）
+  	再一個Verbal 或 Quantitative（30 或 35分鐘作答）
+  	 
+- 各項測驗說明如下：
+  	1. 	分析寫作(Analytical Writing)有兩篇，只能使用電腦打字。
+  	  	一篇Analyze an Issue，30分鐘作答，
+  	  	一篇Analyze an Argument，30分鐘作答。
+  	2. 	語文 (Verbal Reasoning)-- 包含兩個部分，每部分 20題，30分鐘作答。
+  	3. 	計量 (Quantitative Reasoning)-- 包含兩個部分，每部分 20題，35分鐘作答。
+  	  	考計量時，螢幕上有計算機可使用
+  	三項測驗皆可使用擬稿紙與筆，但需在計時當中（即看到題目時），才可使用紙筆擬稿或計算。
+
+    另可能有不計分之Unidentified Section (Verbal或Quantitative)，亦需作答。
+
+
